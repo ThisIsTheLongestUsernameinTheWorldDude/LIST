01b2b3af808260d67ca92fffc3b00382b73b38d4
BATMAN <cpwest11@gmail.com>
2013-01-17 13:01:33 -0600

    PHASE 1 COMPLETE
    AHHH

---
+++ list.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname list) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;117
+
+; A l is one of:
+; – empty
+; – (cons num l)
+; interp. a l represents some amounts of money
+
+
+;adds the con
+(define (sum num l)
+  (cond
+  [(empty? l) num]
+  [else (sum (+ num (first l)) (rest l))]))
